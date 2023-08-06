d40d547a983197d0f7d5acc1f4664519f39dfacd
SuperMax8 <supermax_8@yahoo.fr>
2023-08-06 20:41:09 +0200

    Marche mais c degeu ça graisllle sa mere

--- src/main/java/fr/supermax_8/boostedaudio/Main.java
+++ src/main/java/fr/supermax_8/boostedaudio/Main.java
@@ -2,53 +2,18 @@ package fr.supermax_8.boostedaudio;
 
 import org.glassfish.tyrus.server.Server;
 
-import jakarta.websocket.*;
-import jakarta.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
-
-@ServerEndpoint("/audio")
 public class Main {
 
-
-    private static List<Session> sessions = new ArrayList<>();
-
-    @OnOpen
-    public void onOpen(Session session) {
-        // Ajouter la nouvelle session à la liste
-        sessions.add(session);
-        System.out.println("Nouvelle connexion WebSocket établie");
-    }
-
-    @OnMessage
-    public void onMessage(ByteBuffer audioData, Session session) {
-        // Relayer le flux audio à tous les autres clients connectés
-        for (Session s : sessions) {
-            if (!s.equals(session)) {
-                try {
-                    s.getBasicRemote().sendBinary(audioData);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    @OnClose
-    public void onClose(Session session) {
-        // Supprimer la session de la liste lorsqu'elle se déconnecte
-        sessions.remove(session);
-        System.out.println("Connexion WebSocket fermée");
-    }
+    private static final int PORT = 8080;
 
     public static void main(String[] args) {
-        Server server = new Server("localhost", 11000, "/audio", null, Main.class);
+        Server server = new Server("127.0.0.1", PORT, "/", null, VocalWebSocket.class);
+
         try {
             server.start();
             System.out.println("Serveur WebSocket démarré");
-            Thread.currentThread().join();
+            System.out.println("Appuyez sur Enter pour arrêter le serveur.");
+            System.in.read();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -56,27 +21,5 @@ public class Main {
         }
     }
 
-
-    /*public static void main(String[] args) {
-        startServer();
-    }
-
-    private static void startServer() {
-        Server server = new Server("localhost", 8080, "/audio", null, VocalWebSocket.class);
-
-        try {
-            server.start();
-            System.out.println("Serveur WebSocket démarré sur : " + server.getPort());
-            System.out.println("En attente de connexions...");
-
-            // Pour garder le serveur en cours d'exécution indéfiniment (jusqu'à ce qu'il soit arrêté manuellement)
-            Thread.currentThread().join();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            server.stop();
-        }
-    }*/
 
 }
--- src/main/java/fr/supermax_8/boostedaudio/VocalWebSocket.java
+++ src/main/java/fr/supermax_8/boostedaudio/VocalWebSocket.java
@@ -2,46 +2,52 @@ package fr.supermax_8.boostedaudio;
 
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-@ServerEndpoint("/audio")
+@ServerEndpoint("/")
 public class VocalWebSocket {
 
+    private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
 
-        private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
+    @OnOpen
+    public void onOpen(Session session) {
+        sessions.add(session);
+        System.out.println("Open");
+    }
 
-        @OnOpen
-        public void onOpen(Session session) {
-            sessions.add(session);
-        }
+    @OnMessage
+    public void onMessage(ByteBuffer audioData, Session session) {
+        byte[] bytes = new byte[audioData.remaining()];
+        audioData.get(bytes);
 
-        @OnMessage
-        public void onMessage(byte[] audioData, Session session) {
-            // Traitez l'audioData reçu (par exemple, le diffuser à tous les autres clients connectés)
-            for (Session s : sessions) {
-                if (!s.equals(session)) {
-                    try {
-                        s.getBasicRemote().sendBinary(ByteBuffer.wrap(audioData));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+        // Traitez l'audioData reçu (par exemple, le diffuser à tous les autres clients connectés)
+        System.out.println("Sending...");
+        for (Session s : sessions) {
+            // if (s.equals(session)) continue;
+            try {
+                s.getBasicRemote().sendBinary(ByteBuffer.wrap(bytes));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+    }
 
-        @OnClose
-        public void onClose(Session session) {
-            sessions.remove(session);
-        }
 
-        @OnError
-        public void onError(Throwable t) {
-            t.printStackTrace();
-        }
+    @OnClose
+    public void onClose(Session session) {
+        sessions.remove(session);
+        System.out.println("Closed");
+    }
+
+    @OnError
+    public void onError(Throwable t) {
+        t.printStackTrace();
+    }
 
 
 }
