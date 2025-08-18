f9b698794ba28ca1685b42a84bcee19534abc2f2
SuperMax8 <supermax_8@yahoo.fr>
2025-08-18 17:00:40 +0200

    Fix event api

--- boostedaudio-core/src/main/java/fr/supermax_8/boostedaudio/api/event/EventManager.java
+++ boostedaudio-core/src/main/java/fr/supermax_8/boostedaudio/api/event/EventManager.java
@@ -1,51 +1,46 @@
 package fr.supermax_8.boostedaudio.api.event;
 
-import java.util.EnumMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.UUID;
+import lombok.Getter;
+
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
 
 public class EventManager {
 
+    @Getter
     private static final EventManager instance = new EventManager();
-    private final ConcurrentHashMap<Class<? extends Event>, EnumMap<EventPriority, Listener>> listeners = new ConcurrentHashMap<>();
-
+    // Map<EventClass, Map<Priority, List<Listener>>>
+    private final ConcurrentHashMap<Class<? extends Event>, EnumMap<EventPriority, CopyOnWriteArrayList<Listener>>> listeners = new ConcurrentHashMap<>();
 
     protected EventManager() {
     }
 
-
     public <E extends Event> Listener registerListener(EventPriority priority, Class<E> eventClass, Consumer<E> consumer) {
         Listener listener = new Listener((Consumer<Event>) consumer, UUID.randomUUID());
-        getListeners(eventClass).put(priority, listener);
+        getListeners(eventClass)
+                .computeIfAbsent(priority, p -> new CopyOnWriteArrayList<>()) // Thread-safe list
+                .add(listener);
         return listener;
     }
 
     public void unregisterListener(Listener listener) {
-        listeners.forEach((aClass, eventPriorityListenerEnumMap) -> {
-            Iterator<Map.Entry<EventPriority, Listener>> iterator = eventPriorityListenerEnumMap.entrySet().iterator();
-            while (iterator.hasNext()) {
-                Map.Entry<EventPriority, Listener> entry = iterator.next();
-                if (entry.getValue().getUuid().equals(listener.getUuid())) {
-                    iterator.remove();
-                    break;
-                }
-            }
+        listeners.forEach((aClass, priorityMap) -> {
+            priorityMap.forEach((priority, list) -> list.removeIf(l -> l.getUuid().equals(listener.getUuid())));
         });
     }
 
     public void callEvent(Event event) {
-        getListeners(event.getClass()).forEach((priority, listener) -> listener.getEventConsumer().accept(event));
+        getListeners(event.getClass())
+                .forEach((priority, list) -> {
+                    for (Listener listener : list) {
+                        listener.getEventConsumer().accept(event);
+                    }
+                });
     }
 
-    private EnumMap<EventPriority, Listener> getListeners(Class<? extends Event> eventClass) {
+    private EnumMap<EventPriority, CopyOnWriteArrayList<Listener>> getListeners(Class<? extends Event> eventClass) {
         return listeners.computeIfAbsent(eventClass, aClass -> new EnumMap<>(EventPriority.class));
     }
-
-    public static EventManager getInstance() {
-        return instance;
-    }
-
 }
