5f9c272c17366b8adeb61b9068c175ac0a43a9f3
SuperMax8 <supermax_8@yahoo.fr>
2025-11-12 02:47:02 +0100

    Fix ffmpeg

--- boostedaudio-core/src/main/java/fr/supermax_8/boostedaudio/core/utils/FFmpegUtils.java
+++ boostedaudio-core/src/main/java/fr/supermax_8/boostedaudio/core/utils/FFmpegUtils.java
@@ -1,20 +1,23 @@
 package fr.supermax_8.boostedaudio.core.utils;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import fr.supermax_8.boostedaudio.api.BoostedAudioAPI;
-import net.bramp.ffmpeg.FFmpeg;
-import net.bramp.ffmpeg.FFmpegExecutor;
-import net.bramp.ffmpeg.FFprobe;
-import net.bramp.ffmpeg.builder.FFmpegBuilder;
-import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class FFmpegUtils {
 
-    public static FFmpeg ffmpeg;
-    public static FFprobe ffprobe;
+    public static File ffmpeg;
+    public static File ffprobe;
     public static ConcurrentHashMap<String, Double> durationCache = new ConcurrentHashMap<>();
 
     static {
@@ -23,19 +26,11 @@ public class FFmpegUtils {
             File ffmpegDir = new File(BoostedAudioAPI.getAPI().getConfiguration().getDataFolder(), "ffmpeg");
             String ffmpegPath = ffmpegDir.listFiles((d, s) -> s.contains("ffmpeg"))[0].getAbsolutePath();
             String ffmprobe = ffmpegDir.listFiles((d, s) -> s.contains("ffprobe"))[0].getAbsolutePath();
-            try {
-                ffmpeg = new FFmpeg(ffmpegPath);
-                ffprobe = new FFprobe(ffmprobe);
-                BoostedAudioAPI.getAPI().info("§2FFmpeg initialized!");
-            } catch (Throwable e) {
-                BoostedAudioAPI.getAPI().info("§cError with ffmpeg files");
-                e.printStackTrace();
-            }
+            ffmpeg = new File(ffmpegPath);
+            ffprobe = new File(ffmprobe);
+            BoostedAudioAPI.getAPI().info("§2FFmpeg initialized!");
         } catch (Throwable e) {
             BoostedAudioAPI.getAPI().debug("You don't have ffmpeg installed, guide to install it -> https://supermax-8.gitbook.io/boostedaudio/basics/install-ffmpeg-in-game-audio-volume-modification");
-            if (BoostedAudioAPI.getAPI().getConfiguration().isDebugMode()) {
-                e.printStackTrace();
-            }
         }
     }
 
@@ -45,20 +40,56 @@ public class FFmpegUtils {
         }
     }
 
-    public static void adjustGain(String inputFilePath, String outputFilePath, float gainChange) {
+    public static boolean adjustGain(String inputFilePath, String outputFilePath, float gainChangeDb) {
         warningffmpeg();
+        List<String> command = new ArrayList<>();
+        command.add(ffmpeg.getAbsolutePath());
+        command.add("-i");
+        command.add(inputFilePath);
+        command.add("-af");
+        command.add("volume=" + gainChangeDb + "dB");
+        command.add("-y"); // Overwrite the output file if it exists
+        command.add(outputFilePath);
+
+        BoostedAudioAPI.getAPI().debug("FFMPEG Command execution : " + String.join(" ", command));
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        processBuilder.redirectErrorStream(true);
+
         try {
-            FFmpegBuilder builder = new FFmpegBuilder()
-                    .setInput(inputFilePath)
-                    .setAudioFilter("volume=" + gainChange + "dB")
-                    .addOutput(outputFilePath)
-                    .done();
+            Process process = processBuilder.start();
 
-            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    BoostedAudioAPI.getAPI().debug("FFMPEG: " + line);
+                }
+            }
 
-            executor.createJob(builder).run();
-        } catch (Exception e) {
+            boolean finished = process.waitFor(1, TimeUnit.MINUTES); // Timeout
+            if (!finished) {
+                process.destroy();
+                BoostedAudioAPI.getAPI().debug("FFMPEG Timeout");
+                return false;
+            }
+
+            int exitCode = process.exitValue();
+            if (exitCode == 0) {
+                BoostedAudioAPI.getAPI().debug("FFmpeg change gain success : " + exitCode);
+                return true;
+            } else {
+                BoostedAudioAPI.getAPI().info("FFmpeg end with error : " + exitCode);
+                return false;
+            }
+
+        } catch (IOException e) {
+            BoostedAudioAPI.getAPI().info("Error ffmpeg");
             e.printStackTrace();
+            return false;
+        } catch (InterruptedException e) {
+            BoostedAudioAPI.getAPI().debug("FFmpeg interrupt");
+            Thread.currentThread().interrupt();
+            return false;
         }
     }
 
@@ -78,18 +109,65 @@ public class FFmpegUtils {
         }
     }
 
-    public static double getAudioDurationEx(String audioUrl) throws Exception {
+    public static double getAudioDurationEx(String audioUrl) {
         warningffmpeg();
         Double duration = durationCache.get(audioUrl);
         if (duration != null) return duration;
 
-        // Use FFprobe to probe the file
-        FFmpegProbeResult probeResult = ffprobe.probe(audioUrl);
-
         // Extract duration (in seconds)
-        double d = probeResult.getFormat().duration;
+        double d = computeAudioDuration(audioUrl);
         durationCache.put(audioUrl, d);
         return d;
+    }
+
+    public static double computeAudioDuration(String audioUrl) {
+        String[] command = {
+                ffprobe.getAbsolutePath(),
+                "-v", "quiet",
+                "-print_format", "json",
+                "-show_format",
+                "-show_streams",
+                audioUrl
+        };
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+        try {
+            Process process = processBuilder.start();
+
+            StringBuilder jsonOutput = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    jsonOutput.append(line);
+                }
+            }
+
+            boolean finished = process.waitFor(30, TimeUnit.SECONDS); // Timeout
+            if (!finished || process.exitValue() != 0) {
+                BoostedAudioAPI.getAPI().debug("FFMPEG Timeout");
+                return -1;
+            }
+
+            JsonObject json = new JsonParser().parse(jsonOutput.toString()).getAsJsonObject();
+            JsonObject format = json.getAsJsonObject("format");
+            String durationString = format.get("duration").getAsString();
+
+            return Double.parseDouble(durationString);
+
+        } catch (IOException e) {
+            BoostedAudioAPI.getAPI().debug("FFMPEG Error");
+            e.printStackTrace();
+            return -1;
+        } catch (InterruptedException e) {
+            BoostedAudioAPI.getAPI().debug("FFMPEG interrupt");
+            Thread.currentThread().interrupt();
+            return -1;
+        } catch (Exception e) {
+            BoostedAudioAPI.getAPI().debug("FFPROBE PARSING ERROR PROBABLY!!!");
+            e.printStackTrace();
+            return -1;
+        }
     }
 
 }
